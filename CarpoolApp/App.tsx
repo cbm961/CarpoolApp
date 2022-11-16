e392cae2f8bb24534275bea74162cc6e6727cc3c
yousscharif <youssecharif15@gmail.com>
2022-11-16 22:23:56 +0200

    Created screens directory + finalized temp Loading screen

--- CarpoolApp/App.tsx
+++ CarpoolApp/App.tsx
@@ -30,54 +30,10 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import LoadingScreen from './screens/LoadingScreen'
 
 const App = () => {
-    return (
-      <View style={styles.mainPage}>
-        <View style={styles.headerContent}></View>
-        <View style={styles.mainContent}>
-            <Text style={styles.title}>Carpool Me</Text>
-            <Image source={require('./assets/images/AUB-logo.jpeg')} style={styles.imageLogo} />
-        </View>
-        <View style={styles.footerContent}>
-            <Text style={styles.footerText}> Powered by AUB students </Text>
-        </View>
-      </View>
-    )
-
+    return <LoadingScreen />
 };
-
-const styles = StyleSheet.create({
-  mainPage: {
-    flex: 1,
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    backgroundColor: '#860033',
-  },
-  headerContent: {
-
-  },
-  mainContent: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-  },
-  footerContent: {
-  },
-  imageLogo: {
-    width: 170,
-    height: 170,
-  },
-  title: {
-    color: 'white',
-    fontSize: 30,
-    fontWeight: '600',
-    marginBottom: 20,
-  },
-  footerText: {
-    color: 'white',
-    fontSize: 16,
-  }
-});
 
 export default App;
---
+++ CarpoolApp/screens/LoadingScreen.tsx
@@ -0,0 +1,95 @@
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  ImageBackground,
+  Image,
+  View,
+  TouchableWithoutFeedback,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const LoadingScreen = () => {
+    return (
+      <View style={styles.mainPage}>
+        <View style={styles.headerContent}></View>
+        <View style={styles.mainContent}>
+          <View style={styles.title}>
+            <Text style={styles.titleText}>Carpool Me</Text>
+            <Image source={require('../assets/images/Steering-Wheel-Icon.png')} style={styles.wheelIcon} />
+          </View>
+          <Image source={require('../assets/images/AUB-logo.jpeg')} style={styles.imageLogo} />
+        </View>
+        <View style={styles.footerContent}>
+            <Text style={styles.footerText}> Powered by AUB students </Text>
+        </View>
+      </View>
+    )
+
+};
+
+const styles = StyleSheet.create({
+  mainPage: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#860033',
+  },
+
+  headerContent: {
+
+  },
+
+  mainContent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+
+  footerContent: {
+  },
+
+  imageLogo: {
+    width: 170,
+    height: 170,
+  },
+
+  wheelIcon: {
+    width: 30,
+    height: 30,
+    marginTop: -19,
+    marginLeft: 5,
+  },
+
+  title: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  titleText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '600',
+    marginBottom: 20,
+  },
+
+  footerText: {
+    color: 'white',
+    fontSize: 16,
+  }
+});
+
+export default LoadingScreen;
