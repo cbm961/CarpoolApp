1d601734bf4cd68ac9ce75be40c67d73d9b45915
yousscharif <youssecharif15@gmail.com>
2022-11-16 21:28:01 +0200

    Created UI for loading screen

--- CarpoolApp/App.tsx
+++ CarpoolApp/App.tsx
@@ -16,7 +16,10 @@ import {
   StyleSheet,
   Text,
   useColorScheme,
+  ImageBackground,
+  Image,
   View,
+  TouchableWithoutFeedback,
 } from 'react-native';
 
 import {
@@ -27,94 +30,54 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section: React.FC<
-  PropsWithChildren<{
-    title: string;
-  }>
-> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    return (
+      <View style={styles.mainPage}>
+        <View style={styles.headerContent}></View>
+        <View style={styles.mainContent}>
+            <Text style={styles.title}>Carpool Me</Text>
+            <Image source={require('./assets/images/AUB-logo.jpeg')} style={styles.imageLogo} />
         </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+        <View style={styles.footerContent}>
+            <Text style={styles.footerText}> Powered by AUB students </Text>
+        </View>
+      </View>
+    )
+
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  mainPage: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#860033',
   },
-  sectionTitle: {
-    fontSize: 24,
+  headerContent: {
+
+  },
+  mainContent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+  },
+  footerContent: {
+  },
+  imageLogo: {
+    width: 170,
+    height: 170,
+  },
+  title: {
+    color: 'white',
+    fontSize: 30,
     fontWeight: '600',
+    marginBottom: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  footerText: {
+    color: 'white',
+    fontSize: 16,
+  }
 });
 
 export default App;
