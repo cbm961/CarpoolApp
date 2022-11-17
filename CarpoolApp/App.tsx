d68da845e24b535b61b645ff0fcfb168dba32ea1
yousscharif <youssecharif15@gmail.com>
2022-11-18 00:01:51 +0200

    Added logos + vector images

--- CarpoolApp/App.tsx
+++ CarpoolApp/App.tsx
@@ -8,33 +8,34 @@
  * @format
  */
 
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  ImageBackground,
-  Image,
-  View,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import React from 'react';
+// import {
+//   SafeAreaView,
+//   ScrollView,
+//   StatusBar,
+//   StyleSheet,
+//   Text,
+//   useColorScheme,
+//   ImageBackground,
+//   Image,
+//   View,
+//   TouchableWithoutFeedback,
+// } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
 
-import LoadingScreen from './screens/LoadingScreen'
-import RegisterScreen from './screens/RegisterScreen'
+import LoadingScreen from './screens/LoadingScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import EmailScreen from './screens/Onboarding/EmailScreen';
 
 const App = () => {
-    return <RegisterScreen />
+  return <EmailScreen />;
 };
 
 export default App;
